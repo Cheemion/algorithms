e861d2ec8b7608f900d443cd9e39c4260f162f69
Qimin <wang.qimin@mitigenomics.com>
2017-09-26 16:09:07 +0800

    add another people's implementation of red black tree

--- src/com/algorithms/tree/LeftLeaningRedBlackTree.java
+++ src/com/algorithms/tree/LeftLeaningRedBlackTree.java
@@ -9,7 +9,26 @@ public class LeftLeaningRedBlackTree <K extends Comparable<K>, V>/* implements M
 	private Node root = null;
 	
 	public void put(K k, V v) {
+		root = put(root, k, v);
+		root.color = BLACK;
+	}
+	
+	private Node put(Node cn, K k, V v) {
+		if (cn == null) return new Node(k, v, 1, RED);
+		if(isRed(cn.left) && isRed(cn.right)) split4Node(cn);//是4节点的话 就split
 		
+		int cmp = k.compareTo(cn.k);
+		if (cmp > 0) cn.left = put(cn.left, k, v);
+		else if (cmp < 0) cn.right = put(cn.right, k, v);
+		else cn.v = v; //hit
+		
+		
+		//following code is to fix the tree on the way up
+		if (isRed(cn.right)) cn = rotateLeft(cn);
+		if (isRed(cn.left) && isRed(cn.left.left)) cn = rotateRight(cn);  //变成了一个4节点
+		
+		cn.size = size(cn.left) + size(cn.right) + 1;
+		return cn;
 	}
 	
 	public V get(K k) {
@@ -27,13 +46,50 @@ public class LeftLeaningRedBlackTree <K extends Comparable<K>, V>/* implements M
 	}
 	
 	public void delete(K k) {
+		
+	}
+	
+	public void deleteMax() {
+		root = deleteMax(root);
+		root.color = BLACK;
+	}
+	
+	
+	
+	//make sure currentNode is not a 2Node by make currentNode is red or currentNode.left is red
+	private Node deleteMax(Node cn) {
+		if (isRed(cn.left)) cn = rotateRight(cn); //一定要先rotateRight
+		
+		if (cn.right == null) return null; // approach the end and find the currentNode's childNode is null then just return null;
+		
+		if (!isRed(cn.left) && !isRed(cn.left.right)) { //右节点是子节点　&&  右边子节点是2节点　
+			moveRedRight(cn); // 右边子节点  now it is not 2 node;
+		}
+		
+		cn.left = deleteMax(cn.left);
+		return fixup(cn); 
+	}
+	
+	//used when cn.left is not a 2Node
+	private Node moveRedRight(Node cn) {
+		flipColors(cn);
+		if (isRed(cn.left.left)) { //judge if the  cn.left node is 2 node. 因为有2种strategy 要进行，并且是leftLeaning 所以只需要判断一下cn.left.left就可以了
+			cn = rotateRight(cn); //一种是borrow node from siblings node if siblings is red 
+			flipColors(cn);
+		}
+		//另外一种是 combine parent and sibling node to become a 3 node 也就是 第一句代码flipColors(cn) 语句的功能
+		return cn;
 	}
 	
 	// 判断size的大小
 	private int size(Node node) {
 		if (node == null)
 			return 0;
-		return node.size;
+		return node.size;      
+	}
+	
+	private void split4Node(Node cn) {
+		flipColors(cn);
 	}
 	
 	private void flipColors(Node h) {
@@ -74,17 +130,24 @@ public class LeftLeaningRedBlackTree <K extends Comparable<K>, V>/* implements M
 		x.color = h.color;
 		h.color = RED;
 		
-		x.size = h.size;
+		x.size = h.size; //size is the same
 		h.size = size(h.left) + size(h.right) + 1;
 		
 		return x;
+	}
+	
+	
+	private Node fixup(Node h) {
+		if (isRed(h.right)) h = rotateLeft(h);
+		if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+		if (isRed(h.left) && isRed(h.right)) flipColors(h); //这句话感觉没有必要加　不知道为什么要　split 4 nodes
+		return h;
 	}
 	
 	private boolean isRed(Node x) {
 		if (x == null) return false;
 		return x.color == RED;
 	}
-	
 	
 	private static final boolean RED = true;
 	private static final boolean BLACK = false;
