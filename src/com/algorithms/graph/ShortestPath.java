c04122516be1ba8cb0b57ae7e938913fee591023
Qimin <wang.qimin@mitigenomics.com>
2017-10-26 17:54:21 +0800

    shortestPath

--- src/com/algorithms/graph/ShortestPath.java
+++ src/com/algorithms/graph/ShortestPath.java
@@ -1,17 +1,44 @@
 package com.algorithms.graph;
+
+import com.algorithms.elementary.ArrayStack;
+import com.algorithms.elementary.Stack;
+
 /**
  * find the shortest path in digraph
  * @author altro
  *
  */
 public class ShortestPath {
+	private double[] distTo;
+	private DirectedEdge[] edgeTo;
+	private int source;
+	
 	public ShortestPath(EdgeWeightedDigraph g, int s) {
-		
+		this.source = s;
+		distTo = new double[g.vertices()];
+		edgeTo = new DirectedEdge[g.vertices()];
 	}
+	
 	public boolean hasPathTo(int v) {
 		return false;
 	}
+	
+	public double distTo(int v) {
+		return distTo[v];
+	}
+	
 	public Iterable<DirectedEdge> pathTo(int v) {
-		return null;
+		Stack<DirectedEdge> path = new ArrayStack<>();
+		for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
+			path.push(e);
+		return path;
+	}
+	
+	private void relax(DirectedEdge e) {
+		int v = e.from(), w = e.to();
+		if (distTo[w] > distTo[v] + e.weight()) {
+			distTo[w] = distTo[v] + e.weight();
+			edgeTo[w] = e;
+		}
 	}
 }
