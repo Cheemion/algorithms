284f22337da4e3329fa7376e8bfa5728a25e9f4a
Qimin <wang.qimin@mitigenomics.com>
2017-11-14 17:18:49 +0800

    lsd and msd

---
+++ src/com/algorithms/string/LSD.java
@@ -0,0 +1,59 @@
+package com.algorithms.string;
+
+import java.util.Arrays;
+
+/**
+ * 从字符串的尾部开始排序
+ * @author altro
+ * least significant digit radix sorts, least significant 代表的是 字符串的最后一位
+ */
+public class LSD {
+	
+	public static void main(String[] args) {
+		
+		System.out.println((int)("1".charAt(0)));
+		System.out.println((int)("2".charAt(0)));
+		System.out.println((int)("3".charAt(0)));
+		System.out.println((int)("4".charAt(0)));
+		System.out.println((int)("s".charAt(0)));
+		
+		
+		String s = "222";
+		String s1 = "szs";
+		String s3 = "2dd";
+		String ss[] = new String[3];
+		ss[0] = s;
+		ss[1] = s1;
+		ss[2] = s3;
+		
+		System.out.println(Arrays.toString(ss));
+		sort(ss, 3);
+		System.out.println(Arrays.toString(ss));
+	}
+	
+	public static void sort(String[] a, int w) { //w means fixed-length strings
+		
+		int radix = 256; //2^8
+		int n = a.length;
+		String[] aux = new String[n];
+		
+		for (int d = w - 1; d >= 0; d--) {
+			
+			int[] count = new int[radix + 1];
+			
+			for (int i = 0; i < n; i++)
+				count[a[i].charAt(d) + 1]++;
+			
+			for (int r = 0; r < radix; r++)
+				count[r + 1] += count[r];
+			
+			for (int i = 0; i < n; i++)
+				aux[count[a[i].charAt(d)]++] = a[i];
+			
+			for (int i = 0; i < n; i++)
+				a[i] = aux[i];
+			
+		}
+	}
+	
+}
---
+++ src/com/algorithms/string/MSD.java
@@ -0,0 +1,9 @@
+package com.algorithms.string;
+/**
+ * most-significant-difit-first string sort
+ * @author altro
+ * most-significant means 字符串的第一位
+ */
+public class MSD {
+	
+}
